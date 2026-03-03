1bdeda24253712ecca1d1078802d22c6608d630c
rssXX <31318078+rssXX@users.noreply.github.com>
2026-03-03 22:54:29 +0500

    переносим стили

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,17 @@
 import type {Metadata} from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -14,7 +26,7 @@ export default function RootLayout({
     return (
         <html lang="ru">
         <body
-            className={`antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         {children}
         </body>
