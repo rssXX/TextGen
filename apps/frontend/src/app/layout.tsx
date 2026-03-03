62af63a38ccca9e1b2ca7476bb19263cdf0c516e
rssXX <31318078+rssXX@users.noreply.github.com>
2026-03-03 23:20:40 +0500

    delete Providers

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Providers } from "./providers";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +28,7 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <Providers>{children}</Providers>
+        {children}
         </body>
         </html>
     );
