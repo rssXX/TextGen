e47f538d91acb774f6bbf8905d66747d4e4fa38d
rssXX <31318078+rssXX@users.noreply.github.com>
2026-03-18 15:12:06 +0500

    константа с маршрутами

---
+++ apps/backend/src/consts/routes.ts
@@ -0,0 +1,9 @@
+import {generateRouteV1} from "../routing/v1";
+
+const ROUTES = {
+    'v1': [
+        { handler: generateRouteV1 },
+    ],
+};
+
+export default ROUTES;
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,7 +2,7 @@ import {Elysia} from "elysia";
 import {cors} from '@elysiajs/cors'
 import {openapi} from '@elysiajs/openapi'
 import auth from './auth/auth'
-import {generateRouteV1} from "./routing/v1";
+import {ROUTES} from "./consts";
 
 let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>
 const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema())
@@ -44,12 +44,17 @@ const createServer = async () => {
             paths: await OpenAPI.getPaths()
         }
     }))
-    // TODO: поправить потом
-    app.group('/api', (app) =>
-        app.group('/v1', (app) =>
-            app.use(generateRouteV1)
-        )
-    )
+    app.group('/api', (api) => {
+        for (const [version, definitions] of Object.entries(ROUTES)) {
+            api.group(`/${version}`, (versionGroup) => {
+                for (const route of definitions) {
+                    versionGroup.use(route.handler)
+                }
+                return versionGroup
+            })
+        }
+        return api
+    })
 
     return app;
 }
